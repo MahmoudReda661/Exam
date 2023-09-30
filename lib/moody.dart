50c01525c8c4be8dae69f1f7b0bd69a7f2bb89ef
mahmo <mahmoudreda2002258@gmail.com>
2023-09-30 23:05:48 +0300

    First page completed

--- lib/moody.dart
+++ lib/moody.dart
@@ -9,11 +9,10 @@ class MoodyApp extends StatefulWidget {
 }
 
 class _MoodyAppState extends State<MoodyApp> {
-
   List imageslist = [
-    {"id": 1, "image_path":"assets/images/Group.png"},
-    {"id": 1, "image_path":"assets/images/Group.png"},
-    {"id": 1, "image_path":"assets/images/Group.png"},
+    {"id": 1, "image_path": "assets/images/Frame.png"},
+    {"id": 2, "image_path": "assets/images/Frame.png"},
+    {"id": 3, "image_path": "assets/images/Frame.png"},
   ];
   final CarouselController carouselController = CarouselController();
   int currentIndex = 0;
@@ -21,18 +20,24 @@ class _MoodyAppState extends State<MoodyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: ""),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(40),
         child: Column(children: [
           Row(
             children: [
-              Image.asset("assets/images/Group.png", width: 40, height: 50),
-              SizedBox(width: 10),
+              Image.asset("assets/images/Group.png", width: 50, height: 60),
+              SizedBox(width: 15),
               Text(
                 "Moody",
                 style: TextStyle(
                     color: Colors.black,
-                    fontSize: 25,
+                    fontSize: 30,
                     fontWeight: FontWeight.bold),
               ),
               Spacer(),
@@ -42,22 +47,22 @@ class _MoodyAppState extends State<MoodyApp> {
               ),
             ],
           ),
-          SizedBox(height: 15),
+          SizedBox(height: 20),
           Row(
             children: [
               Text("Hello,",
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 25,
                   )),
               SizedBox(
                 width: 10,
               ),
               Text("Sara Rose",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             ],
           ),
           SizedBox(
-            height: 10,
+            height: 15,
           ),
           Row(
             children: [
@@ -68,7 +73,7 @@ class _MoodyAppState extends State<MoodyApp> {
             ],
           ),
           SizedBox(
-            height: 20,
+            height: 25,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -95,7 +100,7 @@ class _MoodyAppState extends State<MoodyApp> {
               ),
             ],
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 25),
           Row(
             children: [
               Text("Feature",
@@ -110,10 +115,158 @@ class _MoodyAppState extends State<MoodyApp> {
               )
             ],
           ),
-          Spacer(),
+          SizedBox(
+            height: 25,
+          ),
           Stack(
-
-          )
+            children: [
+              InkWell(
+                onTap: () {
+                  print(currentIndex);
+                },
+                child: CarouselSlider(
+                    items: imageslist
+                        .map(
+                          (item) => Image.asset(
+                            item["image_path"],
+                            fit: BoxFit.cover,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        )
+                        .toList(),
+                    carouselController: carouselController,
+                    options: CarouselOptions(
+                        scrollPhysics: BouncingScrollPhysics(),
+                        autoPlay: true,
+                        aspectRatio: 2,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            currentIndex = index;
+                          });
+                        })),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imageslist.asMap().entries.map((entry) {
+                print(entry);
+                print(entry.key);
+                return GestureDetector(
+                  onTap: () => carouselController.animateToPage(entry.key),
+                  child: Container(
+                    width: currentIndex == entry.key ? 17 : 7,
+                    height: 7.0,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 3.0,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: currentIndex == entry.key
+                            ? Colors.red
+                            : Colors.teal),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              Text("Exercise",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              Spacer(),
+              Text("See more",
+                  style: TextStyle(fontSize: 15, color: Colors.green)),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 15,
+                color: Colors.green,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/Frame1.png",
+                      height: 50,
+                      width: 50,
+                    ),
+                    Text(
+                      "Relaxation",
+                      style: TextStyle(fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/Frame2.png",
+                      height: 50,
+                      width: 50,
+                    ),
+                    Text(
+                      "Meditation",
+                      style: TextStyle(fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 35,),
+          Row(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/Frame3.png",
+                      height: 50,
+                      width: 50,
+                    ),
+                    Text(
+                      "Beathing",
+                      style: TextStyle(fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/Frame4.png",
+                      height: 50,
+                      width: 50,
+                    ),
+                    Text(
+                      "Yoga",
+                      style: TextStyle(fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ]),
       ),
     );
